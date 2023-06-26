19aa843f656c3ddfa84bb4c0bdde1fb7b9636b2f
TMozingo13 <TMozingo13@users.noreply.github.com>
2023-06-26 18:02:26 -0500

    routing stuff

---
+++ src/scenes/projects/Fresh.jsx
@@ -0,0 +1,17 @@
+import useMediaQuery from "../../hooks/useMediaQuery";
+import LineGradient from "../../components/LineGradient";
+import { motion } from "framer-motion";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+const Fresh = ({setSelectedPage}) => {
+    const isAboveLarge = useMediaQuery("(min-width: 1060px");
+    return (
+        <div id="fresh">
+            <h1>Fresh</h1>
+            <LineGradient/>
+        </div>
+        
+    );
+};
+
+export default Fresh;
